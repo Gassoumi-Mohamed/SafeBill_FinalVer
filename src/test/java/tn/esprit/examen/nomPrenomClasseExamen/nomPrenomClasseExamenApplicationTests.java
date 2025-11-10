c240e6f66198e4090aa631a61859742ff87fd6e1
Gassoumi-Mohamed <117565334+Gassoumi-Mohamed@users.noreply.github.com>
2025-11-10 15:31:21 +0100

    Disable nomPrenomClasseExamenApplicationTests
    Disable the test class to prevent execution.

--- src/test/java/tn/esprit/examen/nomPrenomClasseExamen/nomPrenomClasseExamenApplicationTests.java
+++ src/test/java/tn/esprit/examen/nomPrenomClasseExamen/nomPrenomClasseExamenApplicationTests.java
@@ -2,6 +2,7 @@ package tn.esprit.examen.nomPrenomClasseExamen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
 @Disabled 
